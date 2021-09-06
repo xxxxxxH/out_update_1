96c6e2c9f459f47dd78231c38b262dbb174b569b
xxxxxxH <425270071@qq.com>
2021-09-06 23:22:13 +0800

    2021年9月6日 23:22:08

--- app/src/main/java/net/basicmodel/MainActivity.kt
+++ app/src/main/java/net/basicmodel/MainActivity.kt
@@ -1,52 +1,37 @@
 package net.basicmodel
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.DownloadManager
 import android.content.Intent
 import android.content.IntentFilter
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.Settings
 import android.text.TextUtils
-import android.util.Log
-import android.view.LayoutInflater
 import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.lijunhuayc.downloader.downloader.DownloadProgressListener
-import com.lijunhuayc.downloader.downloader.DownloaderConfig
 import com.tencent.mmkv.MMKV
-import net.entity.RequestBean
-import net.entity.ResultEntity
 import com.xxxxxxh.http.RequestService
 import com.xxxxxxh.update.ResponseListener
 import com.xxxxxxh.update.UpdateManager
+import net.entity.RequestBean
+import net.entity.ResultEntity
 import net.http.RetrofitUtils
-import okhttp3.MediaType
-import okhttp3.RequestBody
 import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
-import java.io.File
 
-class MainActivity : AppCompatActivity() ,ResponseListener{
+class MainActivity : AppCompatActivity(), ResponseListener {
     val retrofitUtils = RetrofitUtils().retrofit()
     val service = retrofitUtils.create(RequestService::class.java)
     var dialog1: AlertDialog? = null
     var dialog2: AlertDialog? = null
     var progressBar: ProgressBar? = null
-    var manager:UpdateManager? = null
-    var entity:ResultEntity?=null
-    var dialog3:AlertDialog? = null
+    var manager: UpdateManager? = null
+    var entity: ResultEntity? = null
+    var dialog3: AlertDialog? = null
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,7 +40,7 @@ class MainActivity : AppCompatActivity() ,ResponseListener{
         checkState()
         val requestBean = getRequestData()
         manager = UpdateManager.get()
-        manager?.update(AesEncryptUtil.encrypt(Gson().toJson(requestBean)),this)
+        manager?.update(AesEncryptUtil.encrypt(Gson().toJson(requestBean)), this)
         val intentFilter = IntentFilter()
         intentFilter.addAction("action_download")
         intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED)
@@ -63,14 +48,14 @@ class MainActivity : AppCompatActivity() ,ResponseListener{
         registerReceiver(manager?.addReceiver(this), intentFilter)
     }
 
-    private fun checkState(){
-        if (MMKV.defaultMMKV()?.decodeBool("state") == false){
+    private fun checkState() {
+        if (MMKV.defaultMMKV()?.decodeBool("state") == false) {
             return
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
-    private fun getRequestData():RequestBean{
+    private fun getRequestData(): RequestBean {
         val istatus = MMKV.defaultMMKV()!!.decodeBool("isFirst", true)
         val requestBean = RequestBean()
         requestBean.appId = MyApplication().getAppId()
@@ -89,10 +74,10 @@ class MainActivity : AppCompatActivity() ,ResponseListener{
             val resultType = object : TypeToken<ResultEntity>() {}.type
             entity = Gson().fromJson<ResultEntity>(result, resultType)
             if (Build.VERSION.SDK_INT > 24) {
-                dialog1 = manager?.permissionDlg(this,this,entity!!.ukey,entity!!.pkey)
+                dialog1 = manager?.permissionDlg(this, this, entity!!.ukey, entity!!.pkey)
                 dialog1!!.show()
-            }else{
-                dialog3 = manager?.updateDlg(this,entity!!.ikey,entity!!.path)
+            } else {
+                dialog3 = manager?.updateDlg(this, entity!!.ikey, entity!!.path)
                 dialog3!!.show()
             }
         }
@@ -103,10 +88,10 @@ class MainActivity : AppCompatActivity() ,ResponseListener{
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == 1) {
             if (!this.packageManager.canRequestPackageInstalls()) {
-                dialog1 = manager?.permissionDlg(this,this,entity!!.ukey,entity!!.pkey)
+                dialog1 = manager?.permissionDlg(this, this, entity!!.ukey, entity!!.pkey)
                 dialog1!!.show()
-            }else{
-                dialog3 = manager?.updateDlg(this,entity!!.ikey,entity!!.path)
+            } else {
+                dialog3 = manager?.updateDlg(this, entity!!.ikey, entity!!.path)
                 dialog3!!.show()
             }
 
